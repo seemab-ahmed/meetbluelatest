5e87dfd06fcde0a1ad45425d455f68056ed958ab
TalhaSoftPlace <talhanazir410@gmail.com>
2023-05-19 13:59:35 +0500

    change ip to vm

--- app/src/config.js
+++ app/src/config.js
@@ -171,7 +171,7 @@ module.exports = {
                 {
                     ip: '0.0.0.0',
                     announcedIp: '34.131.52.246', // replace by 'public static IPV4 address' https://api.ipify.org (type string --> 'xx.xxx.xxx.xx' not xx.xxx.xxx.xx)
-                    announcedIp: '127.0.0.1', // replace by 'public static IPV4 address' https://api.ipify.org (type string --> 'xx.xxx.xxx.xx' not xx.xxx.xxx.xx)
+                    // announcedIp: '127.0.0.1', // replace by 'public static IPV4 address' https://api.ipify.org (type string --> 'xx.xxx.xxx.xx' not xx.xxx.xxx.xx)
                 }, //announcedIp: '127.0.0.1',  '' will be auto-detected on server start, for docker localPC set '127.0.0.1'
             ], 
             initialAvailableOutgoingBitrate: 1000000,
